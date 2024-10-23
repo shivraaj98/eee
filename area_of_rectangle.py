2cb4956c9a2c04a1031aedd1103f932535071cdd
shivraaj98 <ramjmsshiv@gmail.com>
2024-10-24 00:35:23 +0545

    3rd day

---
+++ area_of_rectangle.py
@@ -0,0 +1,3 @@
+a = int(input("enter lenght of Reactangle"))
+b = int(input("enter breadth of rectangle"))
+print("Area of rectangle is", a*b)
---
+++ odd and even.py
@@ -0,0 +1,10 @@
+a = int(input("Enter a Number"))
+if a>5:
+  if a> 10:
+    print("greatert than 10")
+elif a<5:
+  print("less than 5")
+else:
+  print("it is 5")
+
+  
